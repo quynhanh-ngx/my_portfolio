b0c07b2eff0e2d19effa170a7327e0d426502a05
Anh Quynh Nguyen <quynhhanhh@Anhs-MacBook-Pro.local>
2020-06-06 15:01:58 -0400

    Change favicon and add email icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import linkedIn from './linkedin.svg';
 import github from './github.svg';
+import mail from './mail.svg';
 import logo from './carrot.svg';
 import './App.css';
 
@@ -9,7 +10,7 @@ function App() {
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo"/>
-                <h1>Hi, my name is Quynh Anh</h1>
+                <h1>Hi, my name is Anh Nguyen</h1>
                 <p>
                     I am a Computer Science student at the University of North Carolina at Charlotte.
                 </p>
@@ -45,6 +46,13 @@ function App() {
                     >
                         <div>
                             <img src={github} alt="GitHub logo" className="social-media-logo"/>
+                        </div>
+                    </a>
+                    <a href="mailto:audreynguyen2803@gmail.com"
+                       className="icon-wrapper mail"
+                    >
+                        <div>
+                            <img src={mail} alt="mail logo" className="social-media-logo"/>
                         </div>
                     </a>
                 </div>
