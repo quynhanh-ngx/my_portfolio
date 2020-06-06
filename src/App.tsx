449c15a1931dc7e466f16c78cde02785d5c5e102
Anh Quynh Nguyen <quynhhanhh@Anhs-MacBook-Pro.local>
2020-06-06 13:54:42 -0400

    Add logos for social media and remove margin from headings

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,56 @@
 import React from 'react';
 import logo from './logo.svg';
+import linkedIn from './linkedin.svg';
+import github from './github.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hi, my name is Quynh Anh</h1>
-        <p>
-          I am a Computer Science student at the University of North Carolina at Charlotte.
-        </p>
-        <h2>Languages</h2>
-        <ul>
-          <li>Java</li>
-          <li>Python</li>
-          <li>JavaScript & TypeScript</li>
-        </ul>
-        <h2>Backend</h2>
-        <ul>
-          <li>Spring</li>
-          <li>Django</li>
-          <li>Flask</li>
-        </ul>
-        <h2>Frontend</h2>
-        <ul>
-          <li>React</li>
-          <li>HTML</li>
-          <li>CSS</li>
-        </ul>
-        <a
-          className="App-link"
-          href="https://www.linkedin.com/in/anhnguyenq/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          LinkedIn
-        </a>
-        <a
-        className="App-link"
-        href="https://github.com/quynhanh-ngx"
-        target="_blank"
-        rel="noopener noreferrer"
-        >
-          GitHub
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <h1>Hi, my name is Quynh Anh</h1>
+                <p>
+                    I am a Computer Science student at the University of North Carolina at Charlotte.
+                </p>
+                <h2>Languages</h2>
+                <ul>
+                    <li>Java</li>
+                    <li>Python</li>
+                    <li>JavaScript & TypeScript</li>
+                </ul>
+                <h2>Backend</h2>
+                <ul>
+                    <li>Spring</li>
+                    <li>Django</li>
+                    <li>Flask</li>
+                </ul>
+                <h2>Frontend</h2>
+                <ul>
+                    <li>React</li>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                </ul>
+
+
+                <div className="social-media">
+                    <a href="https://www.linkedin.com/in/anhnguyenq/" target="_blank" rel="noopener noreferrer"
+                       className="icon-wrapper linkedin">
+                        <div>
+                            <img src={linkedIn} alt="LinkedIn logo" className="social-media-logo"/>
+                        </div>
+                    </a>
+                    <a href="https://github.com/quynhanh-ngx" target="_blank" rel="noopener noreferrer"
+                       className="icon-wrapper github"
+                    >
+                        <div>
+                            <img src={github} alt="GitHub logo" className="social-media-logo"/>
+                        </div>
+                    </a>
+                </div>
+            </header>
+        </div>
+    );
 }
 
 export default App;
