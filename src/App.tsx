f18f207a7be106179fb77ac078d5083c4f520a02
Anh Quynh Nguyen <quynhhanhh@Anhs-MacBook-Pro.local>
2020-06-06 12:50:03 -0400

    Add portfolio information to the main page

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,43 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hi, my name is Quynh Anh</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          I am a Computer Science student at the University of North Carolina at Charlotte.
         </p>
+        <h2>Languages</h2>
+        <ul>
+          <li>Java</li>
+          <li>Python</li>
+          <li>JavaScript & TypeScript</li>
+        </ul>
+        <h2>Backend</h2>
+        <ul>
+          <li>Spring</li>
+          <li>Django</li>
+          <li>Flask</li>
+        </ul>
+        <h2>Frontend</h2>
+        <ul>
+          <li>React</li>
+          <li>HTML</li>
+          <li>CSS</li>
+        </ul>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.linkedin.com/in/anhnguyenq/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LinkedIn
+        </a>
+        <a
+        className="App-link"
+        href="https://github.com/quynhanh-ngx"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+          GitHub
         </a>
       </header>
     </div>
