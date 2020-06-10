30179672043ab2752419d7c5909559c256140b52
Anh Quynh Nguyen <quynhhanhh@Anhs-MacBook-Pro.local>
2020-06-10 14:16:55 -0400

    Updates

---
+++ precache-manifest.dec54bffccb7ab6a9badd6b25f9678fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50fad05d4a0125490208e2101778bdc6",
+    "url": "/my_portfolio/index.html"
+  },
+  {
+    "revision": "4f788b4076c15de887b2",
+    "url": "/my_portfolio/static/css/main.2d040a34.chunk.css"
+  },
+  {
+    "revision": "eebf478155b1457ccfe0",
+    "url": "/my_portfolio/static/js/2.37638163.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my_portfolio/static/js/2.37638163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f788b4076c15de887b2",
+    "url": "/my_portfolio/static/js/main.caff3d90.chunk.js"
+  },
+  {
+    "revision": "f471369dddfd9686ecce",
+    "url": "/my_portfolio/static/js/runtime-main.77f2245f.js"
+  },
+  {
+    "revision": "938ecd01745c9b16e240443ae47acff3",
+    "url": "/my_portfolio/static/media/carrot.938ecd01.svg"
+  },
+  {
+    "revision": "0a8a46929d649fb464caddd0ff3a8d10",
+    "url": "/my_portfolio/static/media/github.0a8a4692.svg"
+  },
+  {
+    "revision": "198158b37ea1b645b729aee1fcbc95cc",
+    "url": "/my_portfolio/static/media/linkedin.198158b3.svg"
+  },
+  {
+    "revision": "b955c1835b173105dcd28d0c5b600935",
+    "url": "/my_portfolio/static/media/mail.b955c183.svg"
+  },
+  {
+    "revision": "084d1ac3257abb6dbe616d88c56ec6a2",
+    "url": "/my_portfolio/static/media/stars.084d1ac3.png"
+  }
+]);
